d9d72ad8b8eb51f459ed729a58258ab5c77241f9
rajatgoel <405844+rajatgoel@users.noreply.github.com>
2023-09-13 21:21:47 -0700

    create internal folder

--- itest/frontend_test.go
+++ itest/frontend_test.go
@@ -6,12 +6,12 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/rajatgoel/gh-go/api"
 	frontend_pb "github.com/rajatgoel/gh-go/gen/frontend/v1"
+	"github.com/rajatgoel/gh-go/internal/frontend"
 )
 
 func TestStub(t *testing.T) {
-	h := api.New()
+	h := frontend.New()
 	_, err := h.Stub(context.Background(), &frontend_pb.StubRequest{})
 	require.NoError(t, err)
 }
