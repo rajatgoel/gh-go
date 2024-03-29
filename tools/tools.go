5656ebb440db833a632e24c86a48dccfa5e7dd3e
rajatgoel <405844+rajatgoel@users.noreply.github.com>
2024-03-28 19:20:39 -0700

    gofmt

--- tools/tools.go
+++ tools/tools.go
@@ -3,9 +3,9 @@
 package tools
 
 import (
-        _ "github.com/bufbuild/buf/cmd/buf"
-        _ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-        _ "github.com/sqlc-dev/sqlc/cmd/sqlc"
-        _ "golang.org/x/tools/cmd/deadcode"
-        _ "golang.org/x/vuln/cmd/govulncheck"
+	_ "github.com/bufbuild/buf/cmd/buf"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/sqlc-dev/sqlc/cmd/sqlc"
+	_ "golang.org/x/tools/cmd/deadcode"
+	_ "golang.org/x/vuln/cmd/govulncheck"
 )
