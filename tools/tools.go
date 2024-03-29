11fd48bded07e1a65e4225d48f8c84f1dbe380e7
rajatgoel <405844+rajatgoel@users.noreply.github.com>
2024-03-28 19:19:32 -0700

    fix tools version in go.mod

---
+++ tools/tools.go
@@ -0,0 +1,11 @@
+//go:build tools
+
+package tools
+
+import (
+        _ "github.com/bufbuild/buf/cmd/buf"
+        _ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+        _ "github.com/sqlc-dev/sqlc/cmd/sqlc"
+        _ "golang.org/x/tools/cmd/deadcode"
+        _ "golang.org/x/vuln/cmd/govulncheck"
+)
