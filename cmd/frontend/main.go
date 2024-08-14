f0548447cc6cb721927c9c2678e225d081a3b41b
rajatgoel <405844+rajatgoel@users.noreply.github.com>
2024-08-13 18:49:26 -0700

    Switch to connect rpc

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
-	"net"
+	"log/slog"
+	"net/http"
 
-	"google.golang.org/grpc"
+	"connectrpc.com/grpchealth"
+	"connectrpc.com/grpcreflect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 
 	"github.com/rajatgoel/gh-go/internal/frontend"
 	"github.com/rajatgoel/gh-go/internal/sqlbackend"
-	frontendpb "github.com/rajatgoel/gh-go/proto/frontend/v1"
+	frontendv1connect "github.com/rajatgoel/gh-go/proto/frontend/v1/v1connect"
 )
 
 func main() {
-	port := flag.Int("port", 50051, "The server port")
+	port := flag.Int("port", 5051, "The server port")
 	flag.Parse()
 
 	ctx := context.Background()
@@ -23,16 +28,26 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to create backend: %v", err)
 	}
-	handler := frontend.New(backend)
+	path, handler := frontendv1connect.NewFrontendServiceHandler(frontend.New(backend))
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	mux := http.NewServeMux()
+	mux.Handle(path, handler)
+
+	// health check
+	checker := grpchealth.NewStaticChecker(frontendv1connect.FrontendServiceName)
+	mux.Handle(grpchealth.NewHandler(checker))
+
+	// reflect
+	reflector := grpcreflect.NewStaticReflector(frontendv1connect.FrontendServiceName)
+	mux.Handle(grpcreflect.NewHandlerV1(reflector))
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf("127.0.0.1:%d", *port),
+		Handler: h2c.NewHandler(mux, &http2.Server{}),
 	}
 
-	s := grpc.NewServer()
-	frontendpb.RegisterFrontendServiceServer(s, handler)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	slog.Info("starting server", "port", *port, "path", path)
+	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalf("failed to start server: %v", err)
 	}
 }
--- internal/frontend/handler.go
+++ internal/frontend/handler.go
@@ -3,32 +3,35 @@ package frontend
 import (
 	"context"
 
+	"connectrpc.com/connect"
+
 	"github.com/rajatgoel/gh-go/internal/sqlbackend"
 	frontendpb "github.com/rajatgoel/gh-go/proto/frontend/v1"
+	frontendv1connect "github.com/rajatgoel/gh-go/proto/frontend/v1/v1connect"
 )
 
 type handler struct {
-	frontendpb.UnimplementedFrontendServiceServer
+	frontendv1connect.UnimplementedFrontendServiceHandler
 
 	backend sqlbackend.Backend
 }
 
 func (h *handler) Put(
 	ctx context.Context,
-	req *frontendpb.PutRequest,
-) (*frontendpb.PutResponse, error) {
-	h.backend.Put(ctx, req.Key, req.Value)
-	return &frontendpb.PutResponse{}, nil
+	req *connect.Request[frontendpb.PutRequest],
+) (*connect.Response[frontendpb.PutResponse], error) {
+	h.backend.Put(ctx, req.Msg.Key, req.Msg.Value)
+	return connect.NewResponse(&frontendpb.PutResponse{}), nil
 }
 
 func (h *handler) Get(
 	ctx context.Context,
-	req *frontendpb.GetRequest,
-) (*frontendpb.GetResponse, error) {
-	value := h.backend.Get(ctx, req.Key)
-	return &frontendpb.GetResponse{Value: value}, nil
+	req *connect.Request[frontendpb.GetRequest],
+) (*connect.Response[frontendpb.GetResponse], error) {
+	value := h.backend.Get(ctx, req.Msg.Key)
+	return connect.NewResponse(&frontendpb.GetResponse{Value: value}), nil
 }
 
-func New(backend sqlbackend.Backend) frontendpb.FrontendServiceServer {
+func New(backend sqlbackend.Backend) frontendv1connect.FrontendServiceHandler {
 	return &handler{backend: backend}
 }
